a9cfd164d713b1adf05966d484cd9dac240048f3
mkht <mkht@users.noreply.github.com>
2017-07-24 23:03:49 +0900

    Add sample script

---
+++ Sample/cApplication_Sample.ps1
@@ -0,0 +1,20 @@
+﻿$output = 'C:\MOF'
+
+Configuration cApplication_Sample
+{
+    Import-DscResource -ModuleName DSCR_Application
+
+    cApplication FlashPlayer
+    {
+        Name = 'Adobe Flash Player \d+ NPAPI'   # You can use RegExp when Fuzzy=$true
+        Fuzzy = $true
+        # Download installer from internet.
+        InstallerPath = "http://fpdownload.macromedia.com/pub/flashplayer/latest/help/install_flash_player.exe"
+        Arguments = '-install'
+        NoRestart = $true
+    }
+}
+
+cApplication_Sample -OutputPath $output -ErrorAction Stop
+Start-DscConfiguration -Path $output -Verbose -Wait -Force
+Remove-DscConfigurationDocument -Stage Current, Previous, Pending -Force
