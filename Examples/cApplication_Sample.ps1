175599d0953ceb3b148147ae9c4ad4941ef53456
mkht <mkht@users.noreply.github.com>
2018-09-23 19:33:57 +0900

    Add sample uninstall configuration

--- Examples/cApplication_Sample.ps1
+++ Examples/cApplication_Sample.ps1
@@ -4,13 +4,23 @@ Configuration cApplication_Sample
 {
     Import-DscResource -ModuleName DSCR_Application
 
-    cApplication FlashPlayer
+    cApplication FlashPlayer_Install
     {
         Name = 'Adobe Flash Player \d+ NPAPI'   # You can use RegExp when Fuzzy=$true
         Fuzzy = $true
         # Download installer from internet.
         InstallerPath = "http://fpdownload.macromedia.com/pub/flashplayer/latest/help/install_flash_player.exe"
         Arguments = '-install'
+        NoRestart = $true
+    }
+
+    cApplication AcroRdrDC_Uninstall
+    {
+        Ensure = 'Absent'
+        Name = 'Adobe Acrobat Reader DC'
+        Fuzzy = $true
+        InstallerPath = 'C:\Windows\System32\msiexec.exe'
+        ArgumentsForUninstall = '/X{AC76BA86-7AD7-1041-7B44-AC0F074E4100} /quiet'
         NoRestart = $true
     }
 }
